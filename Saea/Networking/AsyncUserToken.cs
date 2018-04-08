c992f34b4e443b2a6aa7710344fdc8b80f0d5fc0
Mark Laygo <marktlaygo@gmail.com>
2018-04-08 19:09:20 +0800

    Add project files.

---
+++ Saea/Networking/AsyncUserToken.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Saea.Networking
+{
+    class AsyncUserToken
+    {
+        public AsyncUserToken() : this(null)
+        {
+
+        }
+
+        public AsyncUserToken(Socket socket)
+        {
+            Socket = socket;
+        }
+
+        public Socket Socket { get; set; }
+    }
+}
---
+++ Saea/Program.cs
@@ -0,0 +1,25 @@
+﻿using Saea.Networking;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Saea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int maxConnection = 50;
+            int bufferSize = (1024 * 1024) * 1; // 1mb
+            int port = 6275;
+            IPEndPoint ip = new IPEndPoint(IPAddress.Any, port);
+
+            Server server = new Server(maxConnection, bufferSize);
+            server.Init();
+            server.Start(ip);
+        }
+    }
+}
